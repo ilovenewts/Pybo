e40f0684ee4e49d7fd504414b6db14030ec714c9
ilovenewts <iloveamalamute@gmail.com>
2022-01-08 21:42:59 +0900

    first commit

---
+++ pybo/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Question, Answer
+# Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+
+
+admin.site.register(Answer, AnswerAdmin)
